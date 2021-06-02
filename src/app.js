d6dac9ef007caf542d752dfa451994718306fc1a
Bryan Eduria <dev.bryeduria@gmail.com>
2021-06-02 20:24:22 +0800

    added a route and a Dockerfile

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const config = require('./config/envConfig')
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' })
+})
+
 app.listen(config.PORT, () => {
   console.log(
     `The server is now listening at port ${config.PORT}. Have a nice day!`
